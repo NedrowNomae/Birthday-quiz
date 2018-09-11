04ba2d94a788c2ae04e2cb34749da7a245c6d84f
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-09-11 12:27:51 -0400

    Updated from Brython Server: 9/11/2018 12:27:50 PM

--- birthday.py
+++ birthday.py
@@ -33,3 +33,4 @@ Example Session
 """
 name = input("Hello, what is your name?")
 month = input("Hi " + name + ", what was the name of the month you were born in?")
+year = input("And what year were you born in, " +name + "?"?
