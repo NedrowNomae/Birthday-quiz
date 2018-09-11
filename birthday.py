d838374d191a5f62cfb2b441057d5a1659d10dd5
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-09-11 12:26:34 -0400

    Updated from Brython Server: 9/11/2018 12:26:33 PM

--- birthday.py
+++ birthday.py
@@ -32,4 +32,4 @@ Example Session
   Eric, you are a fall baby of the stone age.
 """
 name = input("Hello, what is your name?")
-month = input("Hi" + name + "Eric, what was the name of the month you were born in?"
+month = input("Hi" + name + "Eric, what was the name of the month you were born in?")
