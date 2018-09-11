0727884b315b2c30224fb8bad8a58e427d48c922
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-09-11 12:38:46 -0400

    Updated from Brython Server: 9/11/2018 12:38:46 PM

--- birthday.py
+++ birthday.py
@@ -35,13 +35,17 @@ from datetime import datetime
 from calendar import month_name
 todaymonth = datetime.today().month
 todaydate = datetime.today().day
-month = month_name[todaymonth]
+moonth = month_name[todaymonth]
 print(month)
+print(todaydate)
 name = input("Hello, what is your name?")
 month = input("Hi " + name + ", what was the name of the month you were born in?")
 year = input("And what year were you born in, " +name + "?")
 day = input("And the day?")
-winter = [December,december,January,january,February,february]
-spring = [March,march,April,april,May,may]
-summer = [June,june,July,july,August,august]
-fall = [September,september,October,october,November,november]
+#winter = [December,december,January,january,February,february]
+#spring = [March,march,April,april,May,may]
+#summer = [June,june,July,july,August,august]
+#fall = [September,september,October,october,November,november]
+
+#if moonth == month and day 
+
