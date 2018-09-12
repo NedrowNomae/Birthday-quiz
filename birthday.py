b55929669d443bbb13ba59f0915eda08c6ab1657
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-09-12 09:07:02 -0400

    Updated from Brython Server: 9/12/2018 9:07:01 AM

--- birthday.py
+++ birthday.py
@@ -43,8 +43,11 @@ day = input("And the day?")
 
 if monthb == "October" and day == 31:
     print("You were born on Halloween!")
+if monthb == "October" and day == 31:
+    break
 if str(moonth) == str(monthb) and int(todaydate) == int(day):
     print("Happy birthday!")
+if str(moonth) == str(monthb) and int(todaydate) == int(day):
     break
 if int(year) < int(1980):
     age = "Stone age."
