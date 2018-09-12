47f97fdb1c1d69dd64901392e84252ec91f450e3
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-09-12 08:59:56 -0400

    Updated from Brython Server: 9/12/2018 8:59:56 AM

--- birthday.py
+++ birthday.py
@@ -47,79 +47,13 @@ if str(moonth) == str(monthb) and int(todaydate) == int(day):
     print("Happy birthday!")
 elif int(year) < int(1980):
     age = "Stone age."
-if monthb == "December":
-    season = "winter"
-elif monthb == "January":
-    season = "winter"
-elif monthb == "February":
-    season = "winter"
-elif monthb == "March":
-    season = "spring"
-elif monthb == "April":
-    season = "spring"
-elif monthb == "May":
-    season = "spring"
-elif monthb == "June":
-    season = "summer"
-elif monthb == "July":
-    season = "summer"
-elif monthb == "August":
-    season = "summer"
-else:
-    season = "fall"
-print(name + ", you are a " + season + " baby of the " + age)
-
-elif int(year) <int(1990):
+elif int(year) < int(1990):
     age = "eighties."
-if monthb == "December":
-    season = "winter"
-elif monthb == "January":
-    season = "winter"
-elif monthb == "February":
-    season = "winter"
-elif monthb == "March":
-    season = "spring"
-elif monthb == "April":
-    season = "spring"
-elif monthb == "May":
-    season = "spring"
-elif monthb == "June":
-    season = "summer"
-elif monthb == "July":
-    season = "summer"
-elif monthb == "August":
-    season = "summer"
-else:
-    season = "fall"
-print(name + ", you are a " + season + " baby of the " + age)
-
 elif int(year) < int(2000):
     age = "nineties."
-    if monthb == "December":
-    season = "winter"
-elif monthb == "January":
-    season = "winter"
-elif monthb == "February":
-    season = "winter"
-elif monthb == "March":
-    season = "spring"
-elif monthb == "April":
-    season = "spring"
-elif monthb == "May":
-    season = "spring"
-elif monthb == "June":
-    season = "summer"
-elif monthb == "July":
-    season = "summer"
-elif monthb == "August":
-    season = "summer"
-else:
-    season = "fall"
-print(name + ", you are a " + season + " baby of the " + age)
-
 else:
     age = "two thousands."
-    if monthb == "December":
+if monthb == "December":
     season = "winter"
 elif monthb == "January":
     season = "winter"
